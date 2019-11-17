e2c5b52b903823392861ee61ce250846c0c1d667
Sinan Rassam <sinanrassam@outlook.com>
2019-11-18 11:54:50 +1300

    Updated classes

--- src/AnimalVet/AnimalProcessor.java
+++ src/AnimalVet/AnimalProcessor.java
@@ -1,8 +1,6 @@
 /*
-* To change this license header, choose License Headers in Project Properties.
-* To change this template file, choose Tools | Templates
-* and open the template in the editor.
-*/
+ * A class used by a vet to keep a wait list of animal patients.
+ */
 package AnimalVet;
 
 import java.io.File;
--- src/AnimalVet/VetGUI.java
+++ src/AnimalVet/VetGUI.java
@@ -1,8 +1,7 @@
 /*
-* To change this license header, choose License Headers in Project Properties.
-* To change this template file, choose Tools | Templates
-* and open the template in the editor.
-*/
+ * A GUI which keeps a visual representation of the AnimalProcessor. It displays
+ * the AnimalPatient at the front of the wait list
+ */
 package AnimalVet;
 
 import java.awt.BorderLayout;
